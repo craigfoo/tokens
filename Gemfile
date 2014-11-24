d5829e735d26375a056ba94f6970064b39643148
craig.rettew <craig@zeptoindustries.com>
2014-11-24 17:21:02 -0500

    bundle install, another herkou push

--- Gemfile
+++ Gemfile
@@ -46,3 +46,8 @@ gem 'paperclip', '~> 3.5.4'
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin]
+
+group :production do
+     gem 'pg'
+     gem 'rails_12factor'
+end
--- db/migrate/20141118230507_create_users.rb
+++ db/migrate/20141118230507_create_users.rb
@@ -4,7 +4,6 @@ class CreateUsers < ActiveRecord::Migration
 		t.string "username", :limit => 30
 		t.string "first_name", :limit => 25
 		t.string "last_name", :limit => 50
-		t.string "photo_path", :default => ""
 		t.integer "token_bal", :default => 5
 		t.timestamps
     end
